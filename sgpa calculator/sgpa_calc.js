49bfa9312c3bd4e9fb1225d97ce090c59480458b
shravan <94772842+githubhosting@users.noreply.github.com>
2022-03-26 03:15:16 +0530

    Add files via upload

---
+++ sgpa calculator/sgpa_calc.js
@@ -0,0 +1,65 @@
+var credit = [4, 3, 3, 3, 3, 3, 2, 1];
+
+function gradePoint(mark) {
+  if (mark >= 90) {
+    return 10;
+  } else if (mark >= 80) {
+    return 9;
+  } else if (mark >= 70) {
+    return 8;
+  } else if (mark >= 60) {
+    return 7;
+  } else if (mark >= 50) {
+    return 6;
+  } else if (mark >= 40) {
+    return 4;
+  } else {
+    return 0;
+  }
+}
+
+function validate() {
+  var ma = document.getElementById("i1").value;
+  var py = document.getElementById("i2").value;
+  var el = document.getElementById("i3").value;
+  var ca = document.getElementById("i4").value;
+  var ci = document.getElementById("i5").value;
+  var pl = document.getElementById("i6").value;
+  var en = document.getElementById("i7").value;
+  var dt = document.getElementById("i8").value;
+  var marks = [ma, py, el, ca, ci, pl, en, dt];
+
+  gp = 0;
+  cd = 0;
+  for (var i = 0; i < marks.length; i++) {
+    c = credit[i];
+    m = marks[i];
+    cd += c;
+    gp += c * gradePoint(m * 2);
+  }
+  var sgpa = gp / cd;
+
+  alert("Congratulation! Your CGPA is: " + sgpa.toString());
+}
+
+
+// if(!alertify.myAlert){
+//   //define a new dialog
+//   alertify.dialog('myAlert',function factory(){
+//     return{
+//       main:function(message){
+//         this.message = message;
+//       },
+//       setup:function(){
+//           return { 
+//             buttons:[{text: "cool!", key:27/*Esc*/}],
+//             focus: { element:0 }
+//           };
+//       },
+//       prepare:function(){
+//         this.setContent(this.message);
+//       }
+//   }});
+// }
+// //launch it.
+// alertify.myAlert("Browser dialogs made easy!");
